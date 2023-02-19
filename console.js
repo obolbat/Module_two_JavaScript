956d06ae975a9abed32b2dd9282513e5f499b9c4
E. Bolbat <it-for-bi@yandex.ru>
2023-02-19 22:46:04 +0600

    В задании реализованы расчеты по доходу, проверка по уровню дохода.

--- console.js
+++ console.js
@@ -1,18 +1,29 @@
-let money = 50000,
-    profit = '30000',
-    expenses = 'Питание, Бензин, Еда котикам',
-    purpose = 300000,
-    period = 3;
+const money = prompt('Ваш месячный доход?');
+const purpose = prompt('Сколько вы хотите накопить?');
+let expenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+const amount = prompt('Во сколько обойдуться обязательные статьи расходов?');
+const hasDeposit = true;
 
-console.log('Тип переменной money:', typeof money);
-console.log('Тип переменной profit:', typeof profit);
-console.log(expenses, 'Длина строки expenses:', expenses.length);
-console.log('Период равен', period, 'месяцев');
+const budgetMonth = money - amount;
+console.log('Ваш бюджет на месяц:', budgetMonth);
+console.log('Вы хотите накопить:', purpose);
+console.log('Ваши расходы:', expenses);
+console.log('Сумма расходов:', amount);
 
-let budgetDay = money / 30;
-console.log('Дневной бюджет', budgetDay);
+const budgetDay = Math.floor(budgetMonth / 30);
+const months = Math.ceil(purpose / budgetMonth);
+if (budgetMonth > 0) {
+    console.log(`Вы достигните цели через: ${months} месяца (-ев)`);
+    console.log('Ваш дневной бюджет:', budgetDay);
+} else ('Увы, вы не накопите');
 
-//приводим к нижнему регистру, разбиваем на массив
-expenses = expenses.toLocaleLowerCase();
-let arrayExpenses = expenses.split(', ');
-console.log(arrayExpenses);
+if (budgetDay < 0 ) {
+    console.log('Что-то пошло не так');
+} else if (budgetDay > 6000 ) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay < 3000 && budgetDay < 0 ) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+    console.log('У вас средний уровень дохода');
+}
+
