97fcc54f210a9e5b1c551891360ec042ec3a33cc
E. Bolbat <it-for-bi@yandex.ru>
2023-02-26 21:07:46 +0600

    Добавлены функции расчета накопления за месяц и за какой период будет выполнено накопление. Вывод в console  в соответствии с заданием

--- console.js
+++ console.js
@@ -1,23 +1,33 @@
-const money = prompt('Ваш месячный доход?');
-const purpose = prompt('Сколько вы хотите накопить?');
+const money = +prompt('Ваш месячный доход?');
+const purpose = +prompt('Сколько вы хотите накопить?');
 let expenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-const amount = prompt('Во сколько обойдуться обязательные статьи расходов?');
+let profit = prompt('Перечислите дополнительные виды дохода:');
+let extraMoney = +prompt(`Перечислите возможный доход за ваши дополнительные работы: ${profit}?`);
+const amount = +prompt('Во сколько обойдуться обязательные статьи расходов?');
 const hasDeposit = true;
 
-const budgetMonth = money - amount;
-console.log('Ваш бюджет на месяц:', budgetMonth);
-console.log('Вы хотите накопить:', purpose);
-console.log('Ваши расходы:', expenses);
-console.log('Сумма расходов:', amount);
+const getAccumulatedIncome = (money, amount, extraMoney) => {
+    return money + extraMoney - amount;
+};
 
-const budgetDay = Math.floor(budgetMonth / 30);
-const months = Math.ceil(purpose / budgetMonth);
-if (budgetMonth > 0) {
-    console.log(`Вы достигните цели через: ${months} месяца (-ев)`);
+const accumulatedIncome = getAccumulatedIncome(money, amount, extraMoney);
+const getTargetMonth = (accumulatedIncome, purpose) => {
+    return Math.floor(purpose / accumulatedIncome);
+}
+
+const getBudgetDay = (budgetMonth) => {
+    return (budgetMonth / 30).toFixed(2);
+}
+console.log('Ваш бюджет на месяц:', accumulatedIncome);
+
+const budgetDay = getBudgetDay(accumulatedIncome);
+
+if (accumulatedIncome > 0) {
+    console.log(`Ваша цель накопить ${purpose} с учетом всех ваших расходов цель будет достигнута через`, getTargetMonth(accumulatedIncome, purpose) + ' месяца');
     console.log('Ваш дневной бюджет:', budgetDay);
-} else ('Увы, вы не накопите');
-
-
+} else {
+    console.log('Увы, вы не накопите');
+}
 
 if (budgetDay < 0 ) {
     console.log('Что-то пошло не так');
