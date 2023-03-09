6e93229b8fe986d5fa69a654173683e92058d86c
obolbat <it-for-bi@yandex.ru>
2023-03-09 13:09:29 +0600

    Выполнено задание, результат выведен в  console.log

--- console.js
+++ console.js
@@ -1,40 +1,24 @@
-const money = +prompt('Ваш месячный доход?');
-const purpose = +prompt('Сколько вы хотите накопить?');
-let expenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let profit = prompt('Перечислите дополнительные виды дохода:');
-let extraMoney = +prompt(`Перечислите возможный доход за ваши дополнительные работы: ${profit}?`);
-const amount = +prompt('Во сколько обойдуться обязательные статьи расходов?');
-const hasDeposit = true;
 
-const getAccumulatedIncome = (money, amount, extraMoney) => {
-    return money + extraMoney - amount;
-};
-
-const accumulatedIncome = getAccumulatedIncome(money, amount, extraMoney);
-const getTargetMonth = (accumulatedIncome, purpose) => {
-    return Math.floor(purpose / accumulatedIncome);
+//First part
+const salaries = {John: 100, Ann: 160, Pete: 130};
+let sum = 0;
+for (let key in salaries) {
+    console.log (salaries[key]);
+    sum += salaries[key];
 }
+console.log(sum);
 
-const getBudgetDay = (budgetMonth) => {
-    return (budgetMonth / 30).toFixed(2);
+//Second part
+
+let studentScores = {Коля: '99', Вася: '35', Петя: '70', Таня: '95',Оля: '50', Саша: '20'};
+
+for (const key in studentScores) {
+    if (studentScores[key] < 80) {
+        console.log(`Студент ${studentScores[key]} не проходит отбор`);
+        delete studentScores[key];
+    }
+
 }
-console.log('Ваш бюджет на месяц:', accumulatedIncome);
+console.log(studentScores);
 
-const budgetDay = getBudgetDay(accumulatedIncome);
 
-if (accumulatedIncome > 0) {
-    console.log(`Ваша цель накопить ${purpose} с учетом всех ваших расходов цель будет достигнута через`, getTargetMonth(accumulatedIncome, purpose) + ' месяца');
-    console.log('Ваш дневной бюджет:', budgetDay);
-} else {
-    console.log('Увы, вы не накопите');
-}
-
-if (budgetDay < 0 ) {
-    console.log('Что-то пошло не так');
-} else if (budgetDay > 6000 ) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay < 3000 && budgetDay < 0 ) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else {
-    console.log('У вас средний уровень дохода');
-}
