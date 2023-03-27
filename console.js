67f96d43c08d93cfacef3ff13a91e710c10da18f
obolbat <it-for-bi@yandex.ru>
2023-03-27 17:09:18 +0600

    выполнено задание, 4й пункт реализован двумя способами

--- console.js
+++ console.js
@@ -1,62 +1,37 @@
-let money,
-    purpose,
-    extraMoney,
-    amount;
-//let expenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-//let profit = prompt('Перечислите дополнительные виды дохода:');
-
-const hasDeposit = true;
-
-money = +prompt('Ваш месячный доход?');
-while (isNaN(money)) {
-    money = +prompt('Вы ввели не число. Ваш месячный доход?');
+function getPost() {
+    const url =`https://reqres.in/api/users?per_page=12`;
+    return fetch(url)
+        .then(response => response.json())
 }
 
-purpose = +prompt('Сколько вы хотите накопить?');
-while (isNaN(purpose)) {
-    purpose = +prompt('Вы ввели не число. Сколько вы хотите накопить?');
-}
+getPost()
+    .then(json => {
+        console.log(json);
+        let users = json.data || [];
 
-extraMoney = +prompt(`Перечислите возможный доход за ваши дополнительные работы:`);
-while (isNaN(extraMoney)) {
-    extraMoney = +prompt('Вы ввели не число. Введите дополнительный доход:');
-}
+        users.forEach(item => {
+            console.log(item.last_name);
+        });
 
-amount = +prompt('Во сколько обойдуться обязательные статьи расходов?');
-while (isNaN(amount)) {
-    amount = +prompt('Вы ввели не число. Введите сумму обязательных расходов:');
-}
+        users.filter(item => {
+            return item.last_name[0] === 'F';
+        })
+        .forEach(item => {
+            console.log(item);
+        });
 
-const getAccumulatedIncome = (money, amount, extraMoney) => {
-    return money + extraMoney - amount;
-};
+        //в result храним полученный результат, смотрим, если есть ключ, значит элемент не последний, тогда добавляем запятую, если последний, то ничего не добавляем
+        let text = users.reduce((result, item, $key) => {
+            return result + ($key ? ',' : '') + ' ' + item.last_name + ' ' + item.first_name;
+        }, 'Наша база содержит данныеследующихпользователей:');
+        console.log(text);
 
-const accumulatedIncome = getAccumulatedIncome(money, amount, extraMoney);
-const getTargetMonth = (accumulatedIncome, purpose) => {
-    return Math.floor(purpose / accumulatedIncome);
-};
-//
-const getBudgetDay = (budgetMonth) => {
-    return (budgetMonth / 30).toFixed(2);
-};
-console.log('Ваш бюджет на месяц:', accumulatedIncome);
+        // или вторым вариантом методом map  вывести те же данные фамилию и имя всех пользователей в массиве
+        let newText = users.map(item => item.last_name + ' ' + item.first_name).join(', ');
+        console.log(newText);
 
-const budgetDay = getBudgetDay(accumulatedIncome);
+        let user = users[0];
+        console.log(Object.keys(user));
+    });
 
-if (accumulatedIncome > 0) {
-    console.log(`Ваша цель накопить ${purpose} с учетом всех ваших расходов цель будет достигнута через`, getTargetMonth(accumulatedIncome, purpose) + ' месяца');
-    console.log('Ваш дневной бюджет:', budgetDay);
-} else {
-    console.log('Цель не будет достигнута');
-}
-
-if (budgetDay < 0 ) {
-    console.log('Расходы превышают доходы');
-} else if (budgetDay > 6000 ) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay < 3000 && budgetDay < 0 ) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else {
-    console.log('У вас средний уровень дохода');
-}
 
